71bb6301b69e95c918378d2f1a4897b477560637
WiktorNowak <wnowak2f@gmail.com>
2025-01-03 10:08:29 +0100

    feat: add more cpu operations

--- src/chip8/cpu.rs
+++ src/chip8/cpu.rs
@@ -66,22 +66,36 @@ impl Cpu
             (0x03, ..) => self.op_3xkk(parsed.1, opcode & 0x00FF),
             // SNE: Vx, kk: Skip if not equal.
             (0x04, ..) => self.op_4xkk(parsed.1, opcode & 0x00FF),
-            // LD Vx, kk: Set the value in register.
+            // LD Vx, kk: Vx = kk.
             (0x06, ..) => self.op_6xkk(parsed.1, opcode & 0x00FF),
-            // ADD Vx, kk: Add value to the existing one.
+            // ADD Vx, kk: Vx = Vx + kk.
             (0x07, ..) => self.op_7xkk(parsed.1, opcode & 0x00FF),
             // SE Vx, Vy: Skip if equal.
             (0x05, .., 0x00) => self.op_5xy0(parsed.1, parsed.2),
-            // LD Vx, Vy: Set the value in register.
+            // LD Vx, Vy: Vx = Vy.
             (0x08, .., 0x00) => self.op_8xy0(parsed.1, parsed.2),
-            // OR Vx, Vy: Set the value in register.
+            // OR Vx, Vy: Vx = Vx | Vy.
             (0x08, .., 0x01) => self.op_8xy1(parsed.1, parsed.2),
-            // AND Vx, Vy: Set the value in register.
+            // AND Vx, Vy: Vx = Vx & Vy.
             (0x08, .., 0x02) => self.op_8xy2(parsed.1, parsed.2),
-            // XOR Vx, Vy: Set the value in register.
+            // XOR Vx, Vy: Vx = Vx ^ Vy.
             (0x08, .., 0x03) => self.op_8xy3(parsed.1, parsed.2),
-            // ADD Vx, Vy: Add value to the existing one.
+            // ADD Vx, Vy: Vx = Vx + Vy.
             (0x08, .., 0x04) => self.op_8xy4(parsed.1, parsed.2),
+            // SUB Vx, Vy: Vx = Vx - Vy.
+            (0x08, .., 0x05) => self.op_8xy5(parsed.1, parsed.2),
+            // SHR Vx: Vx >> 1.
+            (0x08, .., 0x06) => self.op_8x06(parsed.1),
+            // SUBN Vx, Vy: Vx = Vy - Vx.
+            (0x08, .., 0x07) => self.op_8xy7(parsed.1, parsed.2),
+            // SHL Vx: Vx << 1.
+            (0x08, .., 0x0E) => self.op_8x0e(parsed.1),
+            // SNE Vx, Vy: Skip if not equal.
+            (0x09, .., 0x00) => self.op_9xy0(parsed.1, parsed.2),
+            // LD I, nnn: I = nnn.
+            (0x0A, ..) => self.op_annn(opcode & 0x0FFF),
+            // JP V0, nnn: PC = V0 + nnn.
+            (0x0B, ..) => self.op_bnnn(opcode & 0x0FFF),
             _ => ExecutionFlow::Next,
         };
 
@@ -198,8 +212,73 @@ impl Cpu
         let result = self.vx[x as usize] as u16 + self.vx[y as usize] as u16;
 
         self.vx[x as usize] = result as u8;
-        self.vx[VF_REGISTER] = if result > u8::MAX as u16 { 1 } else { 0 };
+        self.vx[VF_REGISTER] = (result > u8::MAX as u16) as u8;
 
         ExecutionFlow::Next
+    }
+
+    #[inline]
+    fn op_8xy5(&mut self, x: u16, y: u16) -> ExecutionFlow
+    {
+        let vx = self.vx[x as usize];
+        let vy = self.vx[y as usize];
+
+        self.vx[VF_REGISTER] = (vx > vy) as u8;
+        self.vx[x as usize] = vx.wrapping_sub(vy);
+
+        ExecutionFlow::Next
+    }
+
+    #[inline]
+    fn op_8x06(&mut self, x: u16) -> ExecutionFlow
+    {
+        self.vx[VF_REGISTER] = self.vx[x as usize] & 1;
+        self.vx[x as usize] >>= 1;
+
+        ExecutionFlow::Next
+    }
+
+    #[inline]
+    fn op_8xy7(&mut self, x: u16, y: u16) -> ExecutionFlow
+    {
+        let vx = self.vx[x as usize];
+        let vy = self.vx[y as usize];
+
+        self.vx[VF_REGISTER] = (vy > vx) as u8;
+        self.vx[x as usize] = vy.wrapping_sub(vx);
+
+        ExecutionFlow::Next
+    }
+
+    #[inline]
+    fn op_8x0e(&mut self, x: u16) -> ExecutionFlow
+    {
+        self.vx[VF_REGISTER] = (self.vx[x as usize] & 0x80) >> 7;
+        self.vx[x as usize] <<= 1;
+
+        ExecutionFlow::Next
+    }
+
+    #[inline]
+    fn op_9xy0(&self, x: u16, y: u16) -> ExecutionFlow
+    {
+        if self.vx[x as usize] != self.vx[y as usize]
+        {
+            return ExecutionFlow::SkipNext;
+        }
+        ExecutionFlow::Next
+    }
+
+    #[inline]
+    fn op_annn(&mut self, nnn: u16) -> ExecutionFlow
+    {
+        self.i = nnn;
+        ExecutionFlow::Next
+    }
+
+    #[inline]
+    fn op_bnnn(&self, nnn: u16) -> ExecutionFlow
+    {
+        ExecutionFlow::Jump(self.vx[0] as u16 + nnn)
     }
 }
